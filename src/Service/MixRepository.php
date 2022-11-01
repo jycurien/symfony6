78b4aa8d72f2605e70c4f550cae2f1f7bfcfe301
jycurien <curien.jeanyves@gmail.com>
2022-11-01 15:13:00 +0100

    up

--- src/Service/MixRepository.php
+++ src/Service/MixRepository.php
@@ -5,18 +5,21 @@ namespace App\Service;
 use Psr\Cache\CacheItemInterface;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 class MixRepository
 {
     public function __construct(
         private HttpClientInterface $httpClient,
-        private CacheInterface $cache
+        private CacheInterface $cache,
+        #[Autowire('%kernel.debug%')]
+        private bool $isDebug
     ) {}
 
     public function findAll(): array
     {
         return $this->cache->get('mixes_data', function(CacheItemInterface $cacheItem) {
-            $cacheItem->expiresAfter(5);
+            $cacheItem->expiresAfter($this->isDebug ? 5 : 60);
             $response = $this->httpClient->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json');
 
             return $response->toArray();

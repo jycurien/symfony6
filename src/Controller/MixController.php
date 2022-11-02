545656b5cb6a0ede414c6911670a5fefa7e70649
jycurien <curien.jeanyves@gmail.com>
2022-11-02 12:47:01 +0100

    add vote buttons

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -3,8 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\VinylMix;
-use App\Repository\VinylMixRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -33,11 +33,27 @@ class MixController extends AbstractController
     }
 
     #[Route('/mix/{id}', name: 'app_mix_show')]
-    public function show($id, VinylMixRepository $mixRepository): Response
+    public function show(VinylMix $mix): Response
     {
-        $mix = $mixRepository->find($id);
         return $this->render('mix/show.html.twig', [
             'mix' => $mix,
+        ]);
+    }
+
+    #[Route('/mix/{id}/vote', name: 'app_mix_vote', methods: ['POST'])]
+    public function vote(VinylMix $mix, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $direction = $request->request->get('direction', 'up');
+        if ($direction === 'up') {
+            $mix->setVotes($mix->getVotes() + 1);
+        } else {
+            $mix->setVotes($mix->getVotes() - 1);
+        }
+
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_mix_show', [
+            'id' => $mix->getId(),
         ]);
     }
 }
